77e2e5c7dc587eabb19078e84ed2a61cdb73ed7e
b-valle17 <bryanvalle1217@gmail.com>
2026-07-09 16:05:15 -0500

    Initial commit - Pisip-JBpharmaWeb

---
+++ src/test/java/com/pisip/jbpharmaweb/JbpharmawebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pisip.jbpharmaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbpharmawebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
