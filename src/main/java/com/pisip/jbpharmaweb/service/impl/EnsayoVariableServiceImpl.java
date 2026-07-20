57f53c1b57a74c5df1d5a576b7af12dff703fee7
b-valle17 <bryanvalle1217@gmail.com>
2026-07-20 12:16:09 -0500

    fix: solucionado errores

--- src/main/java/com/pisip/jbpharmaweb/service/impl/EnsayoVariableServiceImpl.java
+++ src/main/java/com/pisip/jbpharmaweb/service/impl/EnsayoVariableServiceImpl.java
@@ -17,27 +17,27 @@ public class EnsayoVariableServiceImpl implements iEnsayoVariableService {
 	}
 
 	public List<EnsayoVariableResponseDto> listar() {
-		return webClient.get().uri("/api/variables").retrieve()
+		return webClient.get().uri("/variables").retrieve()
 				.bodyToMono(new ParameterizedTypeReference<List<EnsayoVariableResponseDto>>() {
 				}).blockOptional().orElseGet(List::of);
 	}
 
 	public Optional<EnsayoVariableResponseDto> buscarPorId(long id) {
-		return webClient.get().uri("/api/variables/{id}", id).retrieve().bodyToMono(EnsayoVariableResponseDto.class)
+		return webClient.get().uri("/variables/{id}", id).retrieve().bodyToMono(EnsayoVariableResponseDto.class)
 				.blockOptional();
 	}
 
 	public EnsayoVariableResponseDto guardar(EnsayoVariableRequestDto dto) {
-		return webClient.post().uri("/api/variables").bodyValue(dto).retrieve().bodyToMono(EnsayoVariableResponseDto.class)
+		return webClient.post().uri("/variables").bodyValue(dto).retrieve().bodyToMono(EnsayoVariableResponseDto.class)
 				.block();
 	}
 
 	public EnsayoVariableResponseDto actualizar(long id, EnsayoVariableRequestDto dto) {
-		return webClient.put().uri("/api/variables/{id}", id).bodyValue(dto).retrieve()
+		return webClient.put().uri("/variables/{id}", id).bodyValue(dto).retrieve()
 				.bodyToMono(EnsayoVariableResponseDto.class).block();
 	}
 
 	public void eliminar(long id) {
-		webClient.delete().uri("/api/variables/{id}", id).retrieve().toBodilessEntity().block();
+		webClient.delete().uri("/variables/{id}", id).retrieve().toBodilessEntity().block();
 	}
 }
